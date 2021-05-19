48144c2e87109bb9d4b65a509b091bc6bb89d160
tungcnn <sontung.cnn@gmail.com>
2021-05-19 14:18:09 +0700

    add database url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,7 @@ export const environment = {
     apiKey: "AIzaSyDTvSWVvpKHs1jBgfzaNfcWVXyPrKg-hEY",
     authDomain: "webmusic-cc387.firebaseapp.com",
     projectId: "webmusic-cc387",
+    databaseUrl: "https://webmusic-cc387-default-rtdb.asia-southeast1.firebasedatabase.app/",
     storageBucket: "webmusic-cc387.appspot.com",
     messagingSenderId: "719548485253",
     appId: "1:719548485253:web:277555fcaad1018c1123ec",
