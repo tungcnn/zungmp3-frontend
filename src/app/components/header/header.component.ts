0419e99c774d1a330a10dc63dc29b3e1cb1360d5
Yumibk82 <ttthang0102@gmail.com>
2021-05-22 17:52:44 +0700

    fixlogin done

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,6 +3,10 @@ import {SongServiceService} from "../../service/song/song-service.service";
 import {NgForm} from "@angular/forms";
 import {Song} from "../../interface/song";
 import {SearchService} from "../../service/search.service";
+import { User } from 'src/app/interface/user';
+import { AuthService } from 'src/app/service/auth/auth.service';
+import { Router } from '@angular/router';
+import { TokenServiceService } from 'src/app/service/token/token-service.service';
 
 @Component({
   selector: 'app-header',
@@ -12,13 +16,64 @@ import {SearchService} from "../../service/search.service";
 export class HeaderComponent implements OnInit {
     songs : Song[] = []
     song: Song = {};
+    formRegistration: any = {};
+  user:User={};
+  isSuccessful =false;
+  isRegistrationFailed = false;
+  errorMessageRegistration = '';
 
-  constructor(private searchService: SearchService) { }
+  formLogin: any = {};
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessageLogin = '';
+
+  constructor(private searchService: SearchService,
+    private authService: AuthService,
+      private router: Router,
+      private tokenStorage: TokenServiceService) { }
 
   ngOnInit() {
+    if(this.tokenStorage.getToken()){
+      this.isLoggedIn = true;
+      // this.roles = this.tokenStorage.getUser().roles;
+    }
   }
 
   search(value: string) {
     this.searchService.searchValue(value)
+  }
+  registration(value : NgForm){
+    this.authService.registration(value.value).subscribe(
+      data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isRegistrationFailed = false;
+      },
+      err => {
+        this.errorMessageRegistration = err.error.message;
+        this.isRegistrationFailed = true;
+      }
+    )
+  }
+
+  login(abc : NgForm){
+    this.authService.login(abc.value).subscribe(
+      data => {
+        this.tokenStorage.saveToken(data.accessToken);
+        this.tokenStorage.saveUser(data);
+
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        // this.roles = this.tokenStorage.getUser().roles;
+        this.reloadPage();
+      },
+      err => {
+        this.errorMessageLogin = err.error.message;
+        this.isLoginFailed = true;
+      }
+    )
+  }
+  reloadPage() {
+    window.location.reload();
   }
 }
