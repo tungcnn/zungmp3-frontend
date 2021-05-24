46fd2d328829eb02112180f5488f415f38ed9839
DuyTran1811 <ferssken99@gmail.com>
2021-05-24 14:28:10 +0700

    Not yet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MusicplayerComponent } from './components/musicplayer/musicplayer.component';
-import { SidemenuComponent } from './components/sidemenu/sidemenu.component';
-import { AddPlayListComponent } from './components/play_list/add-play-list/add-play-list.component';
-import { AddSongComponent } from './components/song/add-song/add-song.component';
-import {AngularFireModule} from "@angular/fire";
-import {environment} from "../environments/environment";
-import {AngularFireStorageModule} from "@angular/fire/storage";
-import {AngularFireDatabaseModule} from "@angular/fire/database";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MusicplayerComponent} from './components/musicplayer/musicplayer.component';
+import {SidemenuComponent} from './components/sidemenu/sidemenu.component';
+import {AddPlayListComponent} from './components/play_list/add-play-list/add-play-list.component';
+import {AddSongComponent} from './components/song/add-song/add-song.component';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../environments/environment';
+import {AngularFireStorageModule} from '@angular/fire/storage';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import { DiscoverComponent } from './components/discover/discover.component';
-import { SearchbarComponent } from './components/searchbar/searchbar.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
-import {CommonModule} from "@angular/common";
-import { SearchMusicComponent } from './components/search-music/search-music.component';
-import { UserComponent } from './components/user/user.component';
-import { AdminComponent } from './components/admin/admin.component';
-import { ProfileComponent } from './components/profile/profile.component';
+import {DiscoverComponent} from './components/discover/discover.component';
+import {SearchbarComponent} from './components/searchbar/searchbar.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {HeaderComponent} from './components/header/header.component';
+import {CommonModule} from '@angular/common';
+import {SearchMusicComponent} from './components/search-music/search-music.component';
+import {UserComponent} from './components/user/user.component';
+import {AdminComponent} from './components/admin/admin.component';
+import {ProfileComponent} from './components/profile/profile.component';
+import {GenresComponent} from './components/genres/genres.component';
 
 @NgModule({
   declarations: [
@@ -38,19 +39,21 @@ import { ProfileComponent } from './components/profile/profile.component';
     UserComponent,
     AdminComponent,
     ProfileComponent,
-    
+    GenresComponent,
+
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HttpClientModule,
-        AngularFireModule.initializeApp(environment.firebaseConfig),
-        AngularFireStorageModule,
-        AngularFireDatabaseModule,
-        FormsModule,
-        CommonModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
+    FormsModule,
+    CommonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
